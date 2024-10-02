526286cfbf4090b1682141398f881c0b75695d5b
Your Name <tomysmile@gmail.com>
2024-10-02 05:31:06 +0000

    feat: document upload

---
+++ polmarkdashboard/polmark_dashboard/doctype/pd_document_upload/exceptions.py
@@ -0,0 +1,13 @@
+import frappe
+
+
+class MaxFileSizeReachedError(frappe.ValidationError):
+	pass
+
+
+class FolderNotEmpty(frappe.ValidationError):
+	pass
+
+
+class FileTypeNotAllowed(frappe.ValidationError):
+	pass
