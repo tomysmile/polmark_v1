4f3bc0e2ef946d76fdea935f8404a806b8f0f062
Your Name <tomysmile@gmail.com>
2024-10-02 06:55:18 +0000

    feat: rekap dpt 2024

---
+++ polmarkdashboard/polmark_dashboard/doctype/pd_rekap_dpt_2024/pd_rekap_dpt_2024.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, thinkspedia and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PDRekapDPT2024(Document):
+	pass
