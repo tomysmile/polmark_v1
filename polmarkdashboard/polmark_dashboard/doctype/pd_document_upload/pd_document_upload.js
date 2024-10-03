39c150ecb098a8d8f38ef962bcdfc0f41ddce52a
Tomy Ismail <tomysmile@gmail.com>
2024-10-03 07:25:04 +0700

    refactor

--- polmarkdashboard/polmark_dashboard/doctype/pd_document_upload/pd_document_upload.js
+++ polmarkdashboard/polmark_dashboard/doctype/pd_document_upload/pd_document_upload.js
@@ -2,28 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("PD Document Upload", {
-	refresh: function (frm) {
-		if (frm.doc.upload_file) {
-			let file_url = frm.doc.upload_file;
+  refresh: function (frm) {
+    if (frm.doc.upload_file) {
+      let file_url = frm.doc.upload_file;
 
-			// Get the file type and size
-			let file_extension = file_url.split(".").pop().toLowerCase();
-			let html = "";
+      // Get the file type and size
+      let file_extension = file_url.split(".").pop().toLowerCase();
+      let html = "";
 
-			if (["jpg", "jpeg", "png", "gif"].includes(file_extension)) {
-				// Preview image
-				html = `<img src="${file_url}" width="100%" />`;
-			} else if (file_extension === "pdf") {
-				// Preview PDF
-				// html = `<embed src="${file_url}" width="100%" height="500px" type="application/pdf">`;
-				html = `<iframe src="${file_url}" width="100%" height="600px"></iframe>`;
-			} else {
-				html = "Unsupported file format";
-			}
+      if (["jpg", "jpeg", "png", "gif"].includes(file_extension)) {
+        // Preview image
+        html = `<img src="${file_url}" width="100%" />`;
+      } else if (file_extension === "pdf") {
+        // Preview PDF
+        html = `<iframe id="pdfIframe" src="${file_url}" width="100%" height="1200px"></iframe>`;
+      } else {
+        html = "Unsupported file format";
+      }
 
-			frm.set_df_property("preview", "options", html);
-		} else {
-			frm.set_df_property("preview", "options", "No file uploaded");
-		}
-	}
+      frm.set_df_property("preview", "options", html);
+    } else {
+      frm.set_df_property("preview", "options", "No file uploaded");
+    }
+  }
 });
