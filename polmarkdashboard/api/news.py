2880958009463e37786052708eea620d233dc0a6
Tomy Ismail <tomysmile@gmail.com>
2024-10-12 20:00:36 +0700

    added variable

--- polmarkdashboard/api/news.py
+++ polmarkdashboard/api/news.py
@@ -1,7 +1,7 @@
 import frappe
-import requests
 
 @frappe.whitelist(allow_guest=True)
-def get_news_links():
-    news_links = frappe.get_all("PD News Link", fields=["name", "title", "summary", "source_url", "news_posted_date", "news_provider", "news_provider_name", "region", "region_code", "headline_image"])
+def get_news_links(region=None):
+    filters = {"region_code": region, "active": 1}
+    news_links = frappe.get_all("PD News Link", filters=filters, fields=["name", "title", "summary", "source_url", "news_posted_date", "news_provider", "news_provider_name", "region", "region_code", "headline_image"])
     return news_links
