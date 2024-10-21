218fc93425ef730948838541d90bd38bc282eab3
Tomy Ismail <tomysmile@gmail.com>
2024-10-21 12:23:01 +0700

    fix fullscreen method

--- polmarkdashboard/public/main.bundle.js
+++ polmarkdashboard/public/main.bundle.js
@@ -3,3 +3,11 @@ import 'leaflet-fullscreen';
 
 import "./js/patch-leaflet.js";
 import "./js/utils.js";
+
+// This ensures that the script runs after the app is fully loaded
+frappe.after_ajax(() => {
+  // This will run after the desk is shown
+  localStorage.container_fullwidth = true;
+  // set fullscreen
+  frappe.ui.toolbar.set_fullwidth_if_enabled();
+});
