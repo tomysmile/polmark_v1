ac5d87371b25cb1f252a1e6fccbf18c08380f7c9
Tomy Ismail <tomysmile@gmail.com>
2024-10-21 12:21:59 +0700

    update script

--- polmarkdashboard/scripts/update_geojson_data.py
+++ polmarkdashboard/scripts/update_geojson_data.py
@@ -67,3 +67,38 @@ def set_active_sumatera_utara():
     # Commit the changes to the database
     frappe.db.commit()
     print("Data updated successfully!")
+
+
+def update_nasional_set_active():
+    doctype = "PD Geojson Indonesia"
+    filters = []
+
+    # execute the function
+    # Fetch all records from PD_GeoJson_Indonesia
+    records = frappe.get_all(doctype, filters=filters, fields=['name'])
+
+    # Iterate over each record and update the 'active' field
+    for record in records:
+        frappe.db.set_value(doctype, record.name, 'active', 1)
+
+    # Commit the changes to the database
+    frappe.db.commit()
+    print("Data updated successfully!")
+
+
+def update_nasional_set_not_active():
+    doctype = "PD Geojson Indonesia"
+    excluded_provinces = ['SUMATERA UTARA', 'DKI JAKARTA', 'JAWA BARAT', 'KALIMANTAN TENGAH', 'KALIMANTAN TIMUR']
+    filters = [['region_name', 'not in', excluded_provinces]]
+
+    # execute the function
+    # Fetch all records from PD_GeoJson_Indonesia
+    records = frappe.get_all(doctype, filters=filters, fields=['name'])
+
+    # Iterate over each record and update the 'active' field
+    for record in records:
+        frappe.db.set_value(doctype, record.name, 'active', 0)
+
+    # Commit the changes to the database
+    frappe.db.commit()
+    print("Data updated successfully!")
