8dddea977ae73c234f3627aa3528da6f65c8b333
Tomy Ismail <tomysmile@gmail.com>
2024-11-01 14:33:39 +0700

    initial dpt kota pekanbaru

---
+++ polmarkdashboard/polmark_dashboard/doctype/pd_dpt_kota_pekanbaru/pd_dpt_kota_pekanbaru_list.js
@@ -0,0 +1,8 @@
+frappe.listview_settings['PD DPT Kota Pekanbaru'] = {
+  refresh: function (listview) {
+    $('span.sidebar-toggle-btn').hide();
+    $('.col-lg-2.layout-side-section').hide();
+  },
+  hide_name_column: true,
+  hide_name_filter: true,
+};
--- polmarkdashboard/scripts/import_dpt.py
+++ polmarkdashboard/scripts/import_dpt.py
@@ -103,3 +103,22 @@ def kab_bekasi():
 
     total_elapsed_time = time.time() - start_time  # Calculate total elapsed time
     print(f"Completed the import process in {total_elapsed_time:.2f} seconds.")
+
+
+def kota_pekanbaru():
+    # Usage
+    file_paths = [
+        frappe.get_app_path("polmarkdashboard", "tmp_data", "dpt_kota_pekanbaru.ndjson")
+    ]
+
+    # Specify the Doctype you are importing data into
+    doctype = "PD DPT Kota Pekanbaru"
+
+    # Start parallel import
+    start_time = time.time()  # Record the overall start time
+    print(f"Starting the import process at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    
+    import_files_in_parallel(file_paths, doctype)
+
+    total_elapsed_time = time.time() - start_time  # Calculate total elapsed time
+    print(f"Completed the import process in {total_elapsed_time:.2f} seconds.")
