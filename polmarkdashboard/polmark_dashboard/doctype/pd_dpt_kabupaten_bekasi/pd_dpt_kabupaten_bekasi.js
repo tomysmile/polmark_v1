c2e8c9ab8c0b1e43802112c70d7e04bb9ef30bf9
Tomy Ismail <tomysmile@gmail.com>
2024-11-04 17:56:18 +0700

    refactor dpt kab bekasi

--- polmarkdashboard/polmark_dashboard/doctype/pd_dpt_kabupaten_bekasi/pd_dpt_kabupaten_bekasi.js
+++ polmarkdashboard/polmark_dashboard/doctype/pd_dpt_kabupaten_bekasi/pd_dpt_kabupaten_bekasi.js
@@ -1,8 +1,49 @@
 // Copyright (c) 2024, thinkspedia and contributors
 // For license information, please see license.txt
+const CONST_PROVINCE_CODE = 32;
+const CONST_PROVINCE_NAME = "JAWA BARAT";
+const CONST_CITY_CODE = 3216;
+const CONST_CITY_NAME = "KABUPATEN BEKASI";
 
-// frappe.ui.form.on("PD DPT Kabupaten Bekasi", {
-// 	refresh(frm) {
+frappe.ui.form.on("PD DPT Kabupaten Bekasi", {
+  refresh(frm) {
+    //
+    frm.set_value('province_code', CONST_PROVINCE_CODE);
+    frm.set_value('province_name', CONST_PROVINCE_NAME);
+    frm.set_value('province_code_bps', CONST_PROVINCE_CODE);
+    frm.set_value('city_code', CONST_CITY_CODE);
+    frm.set_value('city_name', CONST_CITY_NAME);
+    frm.set_value('city_code_bps', CONST_CITY_CODE);
 
-// 	},
-// });
+    frm.set_query('district_code', function () {
+      return {
+        filters: {
+          region_level: 4
+        }
+      };
+    });
+    frm.set_query('sub_district_code', function () {
+      return {
+        filters: {
+          region_level: 5
+        }
+      };
+    });
+  },
+  district_code(frm) {
+    if (frm.doc.district_code) {
+      // Set filter for Sub District field based on selected District
+      frm.set_query('sub_district_code', function () {
+        return {
+          filters: {
+            parent_code: frm.doc.district_code  // Filter sub districts based on selected district
+          }
+        };
+      });
+    } else {
+      frm.set_query('sub_district_code', function () {
+        return {};
+      });
+    }
+  }
+});
--- polmarkdashboard/scripts/import_region.py
+++ polmarkdashboard/scripts/import_region.py
@@ -4,6 +4,7 @@ import json
 import frappe
 from datetime import datetime
 import time
+from pymongo import MongoClient
 
 # Define a function to process a single file and insert the data into Frappe
 def import_file(file_path, doctype):
@@ -70,6 +71,59 @@ def import_files_in_parallel(file_paths, doctype):
         pool.starmap(import_file, [(file_path, doctype) for file_path in file_paths])
 
 
+def import_db(database, collection, doctype):
+    # Connect to MongoDB (local instance)
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client[database]
+    collection = db[collection]
+
+    # Define the Frappe Doctype to which data will be imported
+    doctype = doctype
+
+    # Fetch data from MongoDB collection
+    documents = collection.find()  # Retrieve all documents
+
+    for item in documents:
+        # Construct the Frappe document using the MongoDB data
+        doc = {
+            "doctype": doctype,
+            "region_code": item.get("code"),
+            "region_name": item.get("name"),
+            "region_type": item.get("type"),
+            "region_level": item.get("level"),
+            "region_code_bps": item.get("code_bps"),
+            "region_name_bps": item.get("name_bps"),
+            "parent_code": item.get("parent_code"),
+            "parent_code_bps": item.get("parent_code_bps"),
+            "parent_name": item.get("parent_name"),
+            "parent_type": item.get("parent_type"),
+            "parent_level": item.get("parent_level"),
+            "data_source": item.get("data_source"),
+            "province_name": item.get("p_province_name"),
+            "province_code": item.get("p_province_code"),
+            "province_code_bps": item.get("p_province_code_bps"),
+            "city_name": item.get("p_city_name"),
+            "city_code": item.get("p_city_code"),
+            "city_code_bps": item.get("p_city_code_bps"),
+            "district_name": item.get("p_district_name"),
+            "district_code": item.get("p_district_code"),
+            "district_code_bps": item.get("p_district_code_bps"),
+            "sub_district_name": item.get("p_sub_district_name"),
+            "sub_district_code": item.get("p_sub_district_code"),
+            "sub_district_code_bps": item.get("p_sub_district_code_bps"),
+            "standard": 1
+        }
+    
+        # Insert document into Frappe
+        frappe_doc = frappe.get_doc(doc)
+        frappe_doc.insert(ignore_permissions=True)
+        print(f"Inserted document: {frappe_doc.name}")
+
+    # Commit the transaction to save all records
+    frappe.db.commit()
+
+    print("Data has been successfully imported into Frappe Doctype.")
+
 def kota_bogor():
     # Usage
     file_paths = [
@@ -84,6 +138,22 @@ def kota_bogor():
     print(f"Starting the import process at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     
     import_files_in_parallel(file_paths, doctype)
+
+    total_elapsed_time = time.time() - start_time  # Calculate total elapsed time
+    print(f"Completed the import process in {total_elapsed_time:.2f} seconds.")
+
+
+def kabupaten_bekasi():
+    # Specify the Doctype you are importing data into
+    doctype = "PD Region Kabupaten Bekasi"
+    db = "region"
+    collection = "kabupaten_bekasi"
+
+    # Start parallel import
+    start_time = time.time()  # Record the overall start time
+    print(f"Starting the import process at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    
+    import_db(db, collection, doctype)
 
     total_elapsed_time = time.time() - start_time  # Calculate total elapsed time
     print(f"Completed the import process in {total_elapsed_time:.2f} seconds.")
